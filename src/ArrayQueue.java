6797f021de753856ba609c0b67f80d1197d162b9
shoshosho3 <benhellman80@gmail.com>
2022-06-13 09:41:04 +0300

    End of part A no comments

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,6 +1,11 @@
 import java.lang.reflect.Method;
 import java.util.Iterator;
 
+/**
+ * This class represents a generic queue using an array
+ *
+ * @param <E> class of items in queue
+ */
 public class ArrayQueue<E extends Cloneable> implements Queue<E> {
     private final E[] elements;
     private final int cap;
@@ -9,7 +14,11 @@ public class ArrayQueue<E extends Cloneable> implements Queue<E> {
     private int frontLoops = 0;
     private int rearLoops = 0;
 
-
+    /**
+     * constructor
+     *
+     * @param cap int of maximum size of array
+     */
     public ArrayQueue(int cap) {
         if (cap < 0)
             throw new NegativeCapacityException();
@@ -18,39 +27,69 @@ public class ArrayQueue<E extends Cloneable> implements Queue<E> {
     }
 
 
+    /**
+     * This function inserts an element to emd of queue
+     *
+     * @param element Cloneable element being inserted into queue
+     */
     public void enqueue(Cloneable element) {
         if (size() == cap) {
             throw new QueueOverflowException();
         }
         this.elements[rear] = (E) element;
-        if(rear == cap - 1) rearLoops++;
+        if (rear == cap - 1) rearLoops++;
         rear = (rear + 1) % cap;
     }
 
+    /**
+     * This function removes the top element in queue and returns it
+     *
+     * @return the top element in queue
+     */
     public E dequeue() {
         E element = peek();
         this.elements[front] = null;
-        if(front == cap - 1) frontLoops++;
+        if (front == cap - 1) frontLoops++;
         front = (front + 1) % cap;
         return element;
     }
 
+    /**
+     * This function returns the top element in queue
+     *
+     * @return the top element in queue
+     */
     public E peek() {
         if (size() == 0)
             throw new EmptyQueueException();
         return this.elements[front];
     }
 
+    /**
+     * This function calculates size of queue and returns it
+     *
+     * @return size of queue
+     */
     public int size() {
         if (front < rear || (front == rear && frontLoops == rearLoops))
             return rear - front;
         return rear + cap - front;
     }
 
+    /**
+     * This function checks if queue is empty
+     *
+     * @return true if queue is empty false otherwise
+     */
     public boolean isEmpty() {
         return size() == 0;
     }
 
+    /**
+     * This function creates a clone of this object with cloned elements
+     *
+     * @return clone of this object
+     */
     @Override
     public ArrayQueue<E> clone() {
         try {
@@ -70,6 +109,11 @@ public class ArrayQueue<E extends Cloneable> implements Queue<E> {
         }
     }
 
+    /**
+     * This function returns iterator(enables foreach)
+     *
+     * @return iterator of this object
+     */
     @Override
     public Iterator<E> iterator() {
         return new ArrayQueueIterator<>(elements, front, size());
